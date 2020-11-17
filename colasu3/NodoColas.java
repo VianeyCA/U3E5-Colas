beb4236cc380089343ea59795120f81307f9bbeb
Vianey Ambrosio <71795759+VianeyCA@users.noreply.github.com>
2020-11-17 13:22:46 -0600

    Create NodoColas.java

---
+++ colasu3/NodoColas.java
@@ -0,0 +1,12 @@
+package colasu3;
+
+public class NodoColas {
+    String elemento;
+     NodoColas siguiente;
+
+    public NodoColas(String elemento) {
+        this.elemento = elemento;
+        siguiente=null;
+    }
+    
+}
